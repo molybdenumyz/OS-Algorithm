30b06f9534e25867a6cea10baa96570f1218377b
尹哲 <yinzhe@hrsoft.net>
2017-10-29 17:29:16 +0800

    首次适应法和最佳适应法

--- src/BestFitAlgorithm.py
+++ src/BestFitAlgorithm.py
@@ -1,13 +1,12 @@
 MAX_MEMORY = 640
 
-free_memory_list = [[640,1,640]]
+free_memory_list = [[640, 1, 640]]
 
 
-
-def BF(process_num:int,memory_need:int):
+def BF(process_num: int, memory_need: int):
     start_address = 0
     for i in range(len(free_memory_list)):
-        if(free_memory_list[i][0] >= memory_need):
+        if (free_memory_list[i][0] >= memory_need):
             free_memory_list[i][0] -= memory_need
             start_address = free_memory_list[i][1]
             free_memory_list[i][1] += memory_need
@@ -20,33 +19,37 @@ def BF(process_num:int,memory_need:int):
 
     return start_address
 
-def free(process_num:int,memory_free:int,start_address:int):
+
+def free(process_num: int, memory_free: int, start_address: int):
     len_list = len(free_memory_list)
     for i in range(len(free_memory_list)):
-        if(free_memory_list[i][0] > memory_free):
-            free_memory_list.insert(i,[memory_free,start_address,start_address+memory_free-1])
+        if (free_memory_list[i][0] > memory_free):
+            free_memory_list.insert(i, [memory_free, start_address, start_address + memory_free - 1])
             break
-    if(len_list == len(free_memory_list)):
-        free_memory_list.insert(len(free_memory_list)-1,[memory_free,start_address,start_address+memory_free-1])
+    if (len_list == len(free_memory_list)):
+        free_memory_list.insert(len(free_memory_list) - 1,
+                                [memory_free, start_address, start_address + memory_free - 1])
     print("process:", process_num, "free memory:", memory_free, "success")
     print_partition()
 
-def sort_free_part(start_index:int):
+
+def sort_free_part(start_index: int):
     for i in range(len(free_memory_list)):
-        if(free_memory_list[i][0] > free_memory_list[start_index][0]):
+        if (free_memory_list[i][0] > free_memory_list[start_index][0]):
             temp = free_memory_list.pop(start_index)
-            if(i<start_index):
-                free_memory_list.insert(i,temp)
-            else:#behind start_index this index will -1 after pop
-                free_memory_list.insert(i-1,temp)
+            if (i < start_index):
+                free_memory_list.insert(i, temp)
+            else:  # behind start_index this index will -1 after pop
+                free_memory_list.insert(i - 1, temp)
             break
 
 
 def print_partition():
     print("================================================================")
     for i in free_memory_list:
-       print("memory partition:",i[1],"~",i[2],"size :",i[0],"free")
+        print("memory partition:", i[1], "~", i[2], "size :", i[0], "free")
     print("================================================================\n")
+
 
 if __name__ == '__main__':
     address = [0 for i in range(1, 10)]
