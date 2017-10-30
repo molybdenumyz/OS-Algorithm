60caf8b95fc5ed34ee80cf710aad6d5f1c9a1fc3
尹哲 <yinzhe@hrsoft.net>
2017-10-30 23:34:58 +0800

    最佳适应法，增加内存合并

--- src/BestFitAlgorithm.py
+++ src/BestFitAlgorithm.py
@@ -28,30 +28,25 @@ def BF(process_num: int, memory_need: int):
 
 
 def free(process_num: int, memory_free: int, start_address: int):
-    len_list = len(free_memory_list)
-    for i in range(len(free_memory_list)):
-        if (free_memory_list[i][0] > memory_free):
-            free_memory_list.insert(i, [memory_free, start_address, start_address + memory_free - 1])
-            merge_part(i)
-            break
-    if (len_list == len(free_memory_list)):
-        free_memory_list.insert(len(free_memory_list) - 1,
-                                [memory_free, start_address, start_address + memory_free - 1])
+
+    free_memory_list.insert(len(free_memory_list)-1, [memory_free, start_address, start_address + memory_free - 1])
+    merge_part()
     print("process:", process_num, "free memory:", memory_free, "success")
     print_partition()
 
 
 def sort_free_part(start_index: int):
-    # for i in range(len(free_memory_list)):
-    #     if (free_memory_list[i][0] > free_memory_list[start_index][0]):
-    #         temp = free_memory_list.pop(start_index)
-    #         if (i < start_index):
-    #             free_memory_list.insert(i, temp)
-    #         else:  # behind start_index this index will -1 after pop
-    #             free_memory_list.insert(i - 1, temp)
-    #         break
-    free_memory_list.sort(key = lambda size:size[0])
-    print(free_memory_list)
+    for i in range(len(free_memory_list)):
+        if (free_memory_list[i][0] > free_memory_list[start_index][0]):
+            temp = free_memory_list.pop(start_index)
+            if (i < start_index):
+                free_memory_list.insert(i, temp)
+            else:  # behind start_index this index will -1 after pop
+                free_memory_list.insert(i - 1, temp)
+            break
+
+
+
 
 def sort_by_address():
     print()
@@ -64,39 +59,19 @@ def print_partition():
     print("================================================================\n")
 
 
-def merge_part(i: int):
-
+def merge_part():
     free_memory_list.sort(key=lambda start_address: start_address[1])
 
-    print(free_memory_list)
-    if (free_memory_list[i][1] == free_memory_list[i - 1][2] + 1 and
-                free_memory_list[i][2] == free_memory_list[i + 1][1] - 1):
-        free_memory_list[i][0] += free_memory_list[i - 1][0]
-        free_memory_list[i][0] += free_memory_list[i + 1][0]
-        free_memory_list[i][1] = free_memory_list[i - 1][1]
-        free_memory_list[i][2] = free_memory_list[i + 1][2]
-
-        free_memory_list.pop(i - 1)
-        free_memory_list.pop(i)
-        sort_free_part(i - 1)
-    elif (free_memory_list[i][1] == free_memory_list[i - 1][2] + 1):
-        # print(i)
-        free_memory_list[i][0] += free_memory_list[i - 1][0]
-        # print(free_memory_list)
-        free_memory_list[i][1] = free_memory_list[i - 1][1]
-        # print(free_memory_list)
-        # print(free_memory_list[i])
-        free_memory_list.pop(i - 1)
-        # print(i)
-        # print(free_memory_list)
-        sort_free_part(i)
-        # print(free_memory_list)
-    elif (free_memory_list[i][2] == free_memory_list[i + 1][1] - 1):
-        free_memory_list[i][0] += free_memory_list[i + 1][0]
-        free_memory_list[i][2] = free_memory_list[i + 1][2]
-        free_memory_list.pop(i + 1)
-        sort_free_part(i)
-
+    for i in range(len(free_memory_list) - 1):
+        if(i < len(free_memory_list) -1):
+            if (free_memory_list[i][2] == free_memory_list[i+1][1] -1):
+                free_memory_list[i][2] = free_memory_list[i+1][2]
+                free_memory_list[i][0] += free_memory_list[i+1][0]
+                free_memory_list.pop(i+1)
+    if(free_memory_list[len(free_memory_list) - 1][1] == free_memory_list[len(free_memory_list) - 2][2]+1):
+        free_memory_list[len(free_memory_list - 2)][2] = free_memory_list[len(free_memory_list) -1][2]
+        free_memory_list[len(free_memory_list) - 2][0] += free_memory_list[len(free_memory_list) - 1][0]
+    free_memory_list.sort(key=lambda size: size[0])
 
 if __name__ == '__main__':
     address = [0 for i in range(1, 10)]
